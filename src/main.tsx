4f7f565f53a3c538c183a1053e79f40dbca874bc
dpes31 <dpes31@gmail.com>
2026-06-22 16:06:10 +0900

    feat: install report layout safety guard at app bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { installIframeLayoutSafety } from './lib/installLayoutSafety'
+
+installIframeLayoutSafety()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
