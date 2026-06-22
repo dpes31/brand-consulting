096dd4b3eda11920cd045f4cc57572d53b3eef4b
dpes31 <dpes31@gmail.com>
2026-06-22 16:06:44 +0900

    fix: run iframe structure repair before layout guard

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { installIframePreRepair } from './lib/installIframePreRepair'
 import { installIframeLayoutSafety } from './lib/installLayoutSafety'
 
+installIframePreRepair()
 installIframeLayoutSafety()
 
 createRoot(document.getElementById('root')!).render(
